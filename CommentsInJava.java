b63de77145ead7412b97687e9546b6c371c058c3
Arpita <arpitaistic@gmail.com>
2025-07-16 05:00:53 +0530

    Comments In Java

---
+++ CommentsInJava.java
@@ -0,0 +1,14 @@
+public class CommentsInJava {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("This is a comment example in Java.");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("Comments help explain the code.");
+        
+        // Comments do not affect the execution of the program.
+    }
+}
