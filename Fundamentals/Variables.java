54b59b14ff520344a8aa0189edb7bc959fca7660
Arpita <arpitaistic@gmail.com>
2025-07-16 05:40:55 +0530

    Fundamentals

---
+++ Fundamentals/Variables.java
@@ -0,0 +1,24 @@
+package Fundamentals;
+
+public class Variables {
+    public static void main(String[] args) {
+        //This demonstartes about variables in Java
+        System.out.println("Variables are used to store data in Java.");
+        // Variables can be of different types, such as int, double, String, etc.
+        int myNumber = 10; // Integer variable
+        double myDecimal = 5.5; // Double variable
+        String myText = "Hello, Java!"; // String variable
+        byte myMarks = 100; // Byte variable
+        boolean isPassed = true; // Boolean variable
+        long myLongNumber = 100000L; // Long variable
+        long salary = 1_500_000L; // Long variable with underscores for readability
+        System.out.println("Integer: " + myNumber);
+        System.out.println("Double: " + myDecimal);
+        System.out.println("String: " + myText);
+        System.out.println("Byte: " + myMarks);
+        System.out.println("Boolean: " + isPassed);
+        System.out.println("Long: " + myLongNumber);
+        System.out.println("Salary: " + salary);
+
+    }
+}
